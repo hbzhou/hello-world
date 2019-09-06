1add70577c8b756983459295ec2846f8b7ca71a1
hb.zhou <hb.zhou@itsz.cn>
2019-09-06 16:11:23 +0800

    【comments】Add webservice package

---
+++ java-jdk-sample/src/main/java/com/itsz/java/webservice/WebServiceClient.java
@@ -0,0 +1,27 @@
+package com.itsz.java.webservice;
+
+
+import cn.itsz.HelloWorldService;
+import cn.itsz.HelloWorldService_Service;
+
+public class WebServiceClient {
+
+    public static void main(String[] args) {
+        /**
+         * <service name="helloWorldService">
+         * 获得服务名称
+         */
+        HelloWorldService_Service mywebService = new HelloWorldService_Service();
+
+        /**
+         * <port name="HelloWorldServicePort" binding="tns:HelloWorldServicePortBinding">
+         */
+        HelloWorldService helloWorldService = mywebService.getHelloWorldServicePort();
+
+        /**
+         * 调用方法
+         */
+        System.out.println(helloWorldService.sayHello("webService"));
+
+    }
+}
